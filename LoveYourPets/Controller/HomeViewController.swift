cc7c9d26d1f1538fc92484569222d7b9c36fce41
Neeraj Solanki <niraj.solanki.121@gmail.com>
2022-06-11 01:45:53 +0530

    Assets, Splash, HomeVC SetUp

---
+++ LoveYourPets/Controller/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  LoveYourPets
+//
+//  Created by Niraj Solanki on 11/06/22.
+//
+
+import UIKit
+
+import SwiftUI
+
+class HomeViewController: UIViewController {
+
+    //MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Actions
+    @IBAction func dogAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func catAction(_ sender: UIButton) {
+        
+    }
+    
+}
+
